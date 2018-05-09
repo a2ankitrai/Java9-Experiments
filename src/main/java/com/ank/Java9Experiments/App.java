93337d7f7fbaf7ca9000bb76d7e8935eba58bd62
Rai, Ankit(ankirai) <ankirai@paypal.com>
2018-05-09 15:55:23 +0530

    project created

---
+++ src/main/java/com/ank/Java9Experiments/App.java
@@ -0,0 +1,13 @@
+package com.ank.Java9Experiments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
